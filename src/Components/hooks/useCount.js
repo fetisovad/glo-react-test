bc98d3c415a5eda0032065a7928ebd08bcf9c8fc
Alexandr Fetisov <fetis_94@mail.ru>
2021-12-20 12:00:15 +0500

    end lesson 11

---
+++ src/Components/hooks/useCount.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export function useCount() {
+    const [count, setCount] = useState(1);
+
+    const onChange = (e) => setCount(e.target.value);
+
+    return {
+        count,
+        setCount,
+        onChange,
+    };
+}
